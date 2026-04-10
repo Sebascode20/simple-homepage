ac05166f4e69677a06034264c3d0b5cbf4f3f340
Sebascode20 <sebastiancrack20@outlook.com>
2026-04-10 16:50:08 -0600

    Modify text,headings,hambuguer menu and close menu button color for the dark theme

--- index.js
+++ index.js
@@ -3,7 +3,19 @@ const $checkbox = document.getElementById("open-menu"),
   $btnTheme = document.getElementById("theme-switch"),
   $sunIcon = document.getElementById("sun-icon"),
   $moonIcon = document.getElementById("moon-icon"),
-  $body = document.querySelector("body");
+  $body = document.body,
+  $html = document.documentElement,
+  $text = document.querySelectorAll("p"),
+  $heading = document.querySelector("h1"),
+  $headerLogo = document.querySelector(".header__logo"),
+  $link = document.getElementById("link-demo"),
+  $btnMenu = document.getElementById("hambuger-menu"),
+  $btnClose = document.getElementById("close-button"),
+  $headerNav = document.querySelector(".header__nav"),
+  $headerNavItem = document.querySelectorAll(".header__nav-item");
+
+let darkTextColor =
+  getComputedStyle($html).getPropertyValue("--btn-text-color");
 
 $checkbox.addEventListener("click", function () {
   if (
@@ -13,6 +25,9 @@ $checkbox.addEventListener("click", function () {
     $labelCheckbox
       .querySelector("img")
       .setAttribute("src", "./resources/close-button.svg");
+
+    $labelCheckbox.querySelector("img").setAttribute("id", "close-button");
+
     $labelCheckbox.querySelector("img").setAttribute("alt", "close button");
   } else {
     $labelCheckbox
@@ -25,7 +40,27 @@ $checkbox.addEventListener("click", function () {
 $btnTheme.addEventListener("click", function () {
   $btnTheme.insertAdjacentElement("afterbegin", $sunIcon);
   $body.classList.toggle("dark-theme");
+  $heading.style.color = darkTextColor;
+  $headerNav.classList.add("dark-theme");
+  $link.style.color = darkTextColor;
+  $btnMenu.style.filter = "brightness(0) invert(1)";
 
-  if (!$body.classList.contains("dark-theme"))
+  $text.forEach((p) => {
+    p.style.color = darkTextColor;
+  });
+
+  $headerLogo.setAttribute("src", "./resources/logo-dark.svg");
+
+  if (!$body.classList.contains("dark-theme")) {
     $btnTheme.insertAdjacentElement("afterbegin", $moonIcon);
+    $headerLogo.setAttribute("src", "./resources/logo-light.svg");
+    $heading.removeAttribute("style");
+    $link.removeAttribute("style");
+    $headerNav.classList.remove("dark-theme");
+    $btnMenu.removeAttribute("style");
+
+    $text.forEach((p) => {
+      p.removeAttribute("style");
+    });
+  }
 });
